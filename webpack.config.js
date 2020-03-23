127fe2539ee4b8d2bc70ffb5a612137072439c89
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-22 19:37:50 -0600

    Add manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
+console.log(path.resolve(__dirname, '/src/assets/img/icon.png'));
 
 module.exports = {
   entry: './src/index.js',
@@ -13,43 +16,37 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+      }],
+    },
+    {
+      test: /\.(s*)css$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+      },
+      'css-loader',
+      'sass-loader',
+      ],
+    },
+    {
+      test: /\.(png|gif|jpg|svg)$/,
+      use: [{
+        'loader': 'file-loader',
+        options: {
+          name: 'assets/[hash].[ext]',
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(s*)css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|gif|jpg|svg)$/,
-        use: [
-          {
-            'loader': 'file-loader',
-            options: {
-              name: 'assets/[hash].[ext]',
-            },
-          },
-        ],
-      },
+      }],
+    },
     ],
   },
   devServer: {
@@ -63,6 +60,23 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
     }),
+    new WebpackPwaManifest({
+      short_name: 'Blog',
+      name: 'FrcGustavo - Blog',
+      icons: [{
+        src: './src/assets/img/icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        ios: true,
+      }],
+      ios: true,
+      start_url: '../index.html',
+      scope: '/',
+      display: 'standalone',
+      theme_color: '#ffa500',
+      background_color: '#ffffff',
+      related_applications: [],
+      prefer_related_applications: false,
+    }),
   ],
 };
-
